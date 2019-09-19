8de3400eebeb38ff9f23fbae043e6ee7d9866d02
Gywlssww <tarahjjeon96@gmail.com>
2019-09-19 21:51:00 +0900

    지출 금액 띄우기

--- app/src/main/java/com/example/todayzero/MainFragment.kt
+++ app/src/main/java/com/example/todayzero/MainFragment.kt
@@ -6,11 +6,13 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import com.example.todayzero.db.DBHelper
+import kotlinx.android.synthetic.main.main_frag.*
 
 
 class MainFragment : Fragment() {
 
+    lateinit var dbHelper: DBHelper
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.main_frag, container, false)
@@ -21,5 +23,14 @@ class MainFragment : Fragment() {
         requireActivity().actionBar?.title = resources.getString(R.string.app_name)
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        dbHelper= DBHelper(this.requireContext())
+        init()
+    }
 
+    fun init(){
+       // user정보 등록 후
+        //userBalanceTxt.text=dbHelper.getUser().balance.toString()
+    }
 }
